a4d29eb770edd6931b80830a944bf55d8bf6cff1
Leon Liu <leonliu22@berkeley.edu>
2021-02-20 12:49:17 -0800

    did some work on arduino, drive, not too sure about the functionality of speed controller model

--- models/Arduino.py
+++ models/Arduino.py
@@ -1,2 +1,14 @@
+import smbus 
+
 class Arduino:
-    pass
+
+    def __init__(self):
+        # Slave Addresses for Arduinos 
+        self.SlaveAddress = "fill with arduino address" # I2C Address of Arduino
+
+        # Create the I2C bus 
+        self.I2Cbus = smbus.SMBus(1) 
+
+    def sendToESC(self, bytesToSend):
+
+        self.I2Cbus.write_i2c_block_data(self.SlaveAddress, "Fill register address", bytesToSend)
--- models/Drive.py
+++ models/Drive.py
@@ -1,2 +1,15 @@
 class Drive:
-    pass
+    def __init__(self):
+        pass
+
+    def left(self, deg, mag):
+        pass
+
+    def right(self, deg, mag):
+        pass
+
+    def forward(self, mag):
+        pass
+
+    def stop(self):
+        pass
