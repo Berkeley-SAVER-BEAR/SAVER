a4354ef8095c3ab971c9b7355b1d6c4dae1ac142
Leon Liu <leonliu22@berkeley.edu>
2021-02-21 22:24:46 -0800

    update

--- models/Drive.py
+++ models/Drive.py
@@ -2,14 +2,3 @@ class Drive:
     def __init__(self):
         pass
 
-    def left(self, deg):
-        pass
-
-    def right(self, deg):
-        pass
-
-    def forwardBackward(self, mag):
-        pass
-
-    def stop(self):
-        pass
