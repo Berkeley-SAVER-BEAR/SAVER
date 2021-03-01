cb8ca188f403670fd4f8125c7b060b2de79411dd
Leon Liu <leonliu22@berkeley.edu>
2021-02-28 23:11:57 -0800

    added comments

--- models/speed_controller.py
+++ models/speed_controller.py
@@ -1,16 +1,12 @@
-from . import Arduino
-
+from .arduino import Arduino
 class SpeedController:
-
-    def __init__(self, arduino: Arduino, port: int):
-        self.arduino = arduino
-        self.port = port
 
     # gets and return the PWM based on inputs between -1 and 1
     MAX_FORWARD = 2.71
     MAX_BACKWARDS = 2.90
 
     def getPWM(self, level):
+        # Gets the PWM
         if level < -1 or level > 1:
             return "Incorrect input"
 
@@ -22,10 +18,9 @@ class SpeedController:
             return self.thrustToPWM(level * self.MAX_BACKWARDS)
         
     def thrustToPWM(self, thrust):
+        # fitted a polynomial to make it easy to map thrust to pwm
         if thrust > 0:
             return -32.289500442873752 * thrust ** 2 + 3.869011829925794 * thrust ** 3 + 162.816482640325944 * thrust + 1537.793648693580053
         else:
             thrust *= -1
             return 47.581695898375777 * thrust ** 2 + -7.292251741725251 * thrust ** 3 + -199.610465634680764 * thrust + 1460.832024027741454
-
-
