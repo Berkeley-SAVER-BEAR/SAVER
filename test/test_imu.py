90ab794e408e45510108209ba330022fcc90d1e4
Charles Liu <liu.charles02@gmail.com>
2021-03-02 23:41:29 -0800

    update test_imu to also print out current time

--- test/test_imu.py
+++ test/test_imu.py
@@ -1,10 +1,12 @@
 import time
+from datetime import datetime
 from models import Imu
 
 
 def test_imu():
     """Tests all sensors of IMU."""
     while True:
+        print(datetime.now().time())
         print("Accelerometer (m/s^2): {}".format(imu.get_acceleration()))
         print("Gyroscope (rad/sec): {}".format(imu.get_gyro()))
         print("Euler angle: {}".format(imu.get_euler_angles()))
@@ -16,36 +18,42 @@ def test_imu():
 
 def test_accelerometer():
     while True:
+        print(datetime.now().time())
         print("Accelerometer (m/s^2): {}".format(imu.get_acceleration()))
         time.sleep(1)
 
 
 def test_gyroscope():
     while True:
+        print(datetime.now().time())
         print("Gyroscope (rad/sec): {}".format(imu.get_gyro()))
         time.sleep(1)
 
 
 def test_euler_angles():
     while True:
+        print(datetime.now().time())
         print("Euler angle: {}".format(imu.get_euler_angles()))
         time.sleep(1)
 
 
 def test_quaternion():
     while True:
+        print(datetime.now().time())
         print("Quaternion: {}".format(imu.get_quaternion()))
         time.sleep(1)
 
 
 def test_linear_acceleration():
     while True:
+        print(datetime.now().time())
         print("Linear acceleration (m/s^2): {}".format(imu.get_linear_acceleration()))
         time.sleep(1)
 
 
 def test_gravity():
     while True:
+        print(datetime.now().time())
         print("Gravity (m/s^2): {}".format(imu.get_gravity()))
         time.sleep(1)
 
