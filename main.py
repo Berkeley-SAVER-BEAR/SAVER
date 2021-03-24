bbeb4e4d6ecf5ba7e68536aa1f999914800b4a7b
Leon Liu <leonliu22@berkeley.edu>
2021-03-23 21:39:54 -0700

    saved main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from robot import Robot
-from models import Arduino, Drive, Imu, KerberosSDR, SpeedController
+from models import Arduino, Drive, KerberosSDR, SpeedController
 from constants import *
 import time
 
