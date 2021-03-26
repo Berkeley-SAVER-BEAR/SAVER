41625b9ba7a7448e3cc15d1502ab23966b91f0f0
Leon Liu <leonliu22@berkeley.edu>
2021-03-26 14:09:01 -0700

    update run_imu for more optimized thrusters

---
+++ models/run_imu.py
@@ -0,0 +1,37 @@
+import . from imu
+
+thisImu = Imu()
+
+dt = 0.1
+t = 0
+
+
+#loop this (MainLoop)
+
+acelVector = get_acceleration(self)
+t = t + dt
+
+if t < 2:
+    x = 0
+    y = 0
+    z = 0
+    xD = 0
+    yD = 0
+    zD = 0
+    xDDCorr = acelVector[0]
+    yDDCorr = acelVector[1]
+    zDDCorr = acelVector[2]
+else
+    xDD = acelVector[0] - xDDCorr
+    yDD = acelVector[1] - yDDCorr
+    zDD = acelVector[2] - zDDCorr
+
+    xD = xD + xDD * dt
+    yD = yD + yDD * dt
+    zD = zD + zDD * dt
+
+    x = x + xD * dt
+    y = y + yD * dt
+    z = z + zD * dt
+
+desiredV
