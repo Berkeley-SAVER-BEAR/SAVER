2d7529468299b1e5c7be673b5ba083057be40046
Leon753 <leonliu1839@gmail.com>
2021-03-26 17:58:38 -0700

    make manualtest into a class

--- manual_test_accelerate.py
+++ manual_test_accelerate.py
@@ -3,41 +3,59 @@ from models import Arduino, Drive, KerberosSDR, SpeedController
 from constants import *
 import time
 
-arduino = Arduino()
-thrusters = SpeedController(arduino)
-drive = Drive(thrusters)
-#imu = Imu()
-radio = KerberosSDR()
+def initialize():
+    arduino = Arduino()
+    thrusters = SpeedController(arduino)
+    drive = Drive(thrusters)
+    #imu = Imu()
+    radio = KerberosSDR()
+    return drive, radio, arduino
     
-
-def accelerate_test(duration):
-    # start with scale of 0.1, increment by 0.3 for each iteration
-    for x in range(0.1, 1.1, 0.3):
-        # keeps current speed for duration
-        drive.tank_drive(1, 1, x)
-        time.sleep(duration)
-    drive.drive.tank_drive(0, 0, 0)
-    time.sleep(3)
-
-
-def decelerate_test(duration):
-    # start with scale of 1, decrement by 0.3 for each iteration
-    for x in range(1, 0.0, -0.3):
-        drive.tank_drive(1, 1, x)
-        # keeps current speed for duration
-        time.sleep(duration)
-    drive.drive.tank_drive(0, 0, 0)
-    time.sleep(3)
-
-    
-def manualtest():
+def main():
+    drive, radio, arduino = initialize()
     robot = Robot(drive, radio, arduino)
+    test_scale = 0.1
+    duration = 3
+    turntime = 2
+
+    for i in range(8):
+        drive.tank_drive(0, 0, test_scale)
+
+    #accelerate_test(drive, duration)
+    #decelerate_test(drive, duration)
+    manualtest(drive, duration)
+
+def accelerate_test(drive, duration):
+    # start with scale of 0.1, increment by 0.3 for each iteration
+    drive.tank_drive(0, 0, duration)
+    time.sleep(3)
+    for i in range(1, 10, 3):
+        x = i / 10
+        # keeps current speed for duration
+        drive.tank_drive(1, 1, x)
+        time.sleep(duration)
+    drive.tank_drive(0, 0, 0)
+
+
+def decelerate_test(drive, duration):
+    # start with scale of 1, decrement by 0.3 for each iteration
+    drive.tank_drive(0, 0, duration)
+    time.sleep(3)
+    for i in range(1, 10, 3):
+        x = (11 - i) / 10
+        drive.tank_drive(1, 1, x)
+        # keeps current speed for duration
+        time.sleep(duration)
+    drive.tank_drive(0, 0, 0)
+
+    
+def manualtest(drive, duration):
     
     # test acceleration and deceleration
-    robot.run(accelerate_test(1))
-    time.sleep(2)
-    robot.run(decelerate_test(1))
+    accelerate_test(drive, duration)
+    time.sleep(3)
+    decelerate_test(drive, duration)
 
 
-if __name__ == "__manualtest__":
-    manualtest()
+if __name__ == "__main__":
+    main()
--- manualtest.py
+++ manualtest.py
@@ -3,103 +3,115 @@ from models import Arduino, Drive, KerberosSDR, SpeedController
 from constants import *
 import time
 
-def initialize():
-    arduino = Arduino()
-    thrusters = SpeedController(arduino)
-    drive = Drive(thrusters)
-    #imu = Imu()
-    radio = KerberosSDR()
+TEST_SCALE = 0.1
+DURATION = 3
+TURNTIME = 2
 
-    return drive, radio, arduino
+class ManualTest:
+    def __init__(self):
+        self.arduino = Arduino()
+        self.thrusters = SpeedController(self.arduino)
+        self.drive = Drive(self.thrusters)
+        #imu = Imu()
+        self.radio = KerberosSDR()
+        
+        self.robot = Robot(self.drive, self.radio, self.arduino)
 
-def main():
-    drive, radio, arduino = initialize()
-    robot = Robot(drive, radio, arduino)
-    test_scale = 0.1
-    duration = 3
-    turntime = 2
+        for i in range(8):
+            self.drive.tank_drive(0, 0, TEST_SCALE)
 
-    for i in range(8):
-        drive.tank_drive(0, 0, test_scale)
+        #return drive, radio, arduino
 
-    #uncomment depending on what test we want
+    #def main():
+        #drive, radio, arduino = initialize()
+        #robot = Robot(drive, radio, arduino)
+        #test_scale = 0.1
+        #duration = 3
+        #turntime = 2
 
-    #turnrighttest(drive, duration, test_scale)
-    #turnlefttest(drive, duration, test_scale)
-    #straighttestparams(drive, duration, test_scale)
-    turntest(drive)
-    #straighttest(drive, duration, test_scale, turntime)
-    #straightspeed(drive, duration, test_scale, turntime)
-    
-    # for timing purposes
-    #t = returntime()
-    #print(time.asctime(time.localtime(t)))
+        #for i in range(8):
+        #    self.drive.tank_drive(0, 0, test_scale)
 
-    
-def turnrighttest(drive, duration, scale):
-    drive.tank_drive(0, 0, scale)
-    time.sleep(duration)
-    drive.tank_drive(0.5, 0, scale)
-    #self.drive.tank_drive(1, 0.2473, scale)
-    #self.drive.tank_drive(1, 0.6, scale)
-    #self.drive.tank_drive(1, 0.8124, scale)
-    #self.drive.tank_drive(1, 0.9156, scale)
-    #self.drive.tank_drive(1, 0.9572, scale)
+        #uncomment depending on what test we want
 
-    time.sleep(duration)
-    drive.tank_drive(0, 0, scale)
-    time.sleep(3)
+        #turnrighttest(duration, test_scale)
+        #turnlefttest(duration, test_scale)
+        #straighttestparams(duration, test_scale)
+        #turntest()
+        #straighttest(duration, test_scale, turntime)
+        #straightspeed(duration, test_scale, turntime)
+        
+        # for timing purposes
+        #t = returntime()
+        #print(time.asctime(time.localtime(t)))
 
-def turnlefttest(drive, duration, scale):
-    drive.tank_drive(0, 0, scale)
-    time.sleep(duration)
-    drive.tank_drive(0, 0.5, scale)
-    #self.drive.tank_drive(0.2473, 1, scale)
-    #self.drive.tank_drive(0.6, 1, scale)        
-    #self.drive.tank_drive(0.8124, 1, scale)
-    #self.drive.tank_drive(0.9156, 1, scale)        
-    #self.drive.tank_drive(0.9572, 1, scale)
-    time.sleep(duration)
-    drive.tank_drive(0, 0, scale)
-    time.sleep(3)
+    def turnrighttest(self, duration=DURATION, scale=TEST_SCALE):
+        self.drive.tank_drive(0, 0, scale)
+        time.sleep(duration)
+        self.drive.tank_drive(0.5, 0, scale)
+        #self.drive.tank_drive(1, 0.2473, scale)
+        #self.drive.tank_drive(1, 0.6, scale)
+        #self.drive.tank_drive(1, 0.8124, scale)
+        #self.drive.tank_drive(1, 0.9156, scale)
+        #self.drive.tank_drive(1, 0.9572, scale)
 
-def straighttestparams(drive, duration, scale):
-    drive.tank_drive(0, 0, scale)
-    time.sleep(duration)
-    drive.tank_drive(1, 1, scale)
-    time.sleep(duration)
-    drive.tank_drive(0, 0, scale)
-    time.sleep(3)
+        time.sleep(duration)
+        self.drive.tank_drive(0, 0, scale)
+        time.sleep(3)
 
-def returntime():
-    return time.time()
-    
+    def turnlefttest(self, duration=DURATION, scale=TEST_SCALE):
+        self.drive.tank_drive(0, 0, scale)
+        time.sleep(duration)
+        self.drive.tank_drive(0, 0.5, scale)
+        #self.drive.tank_drive(0.2473, 1, scale)
+        #self.drive.tank_drive(0.6, 1, scale)        
+        #self.drive.tank_drive(0.8124, 1, scale)
+        #self.drive.tank_drive(0.9156, 1, scale)        
+        #self.drive.tank_drive(0.9572, 1, scale)
+        time.sleep(duration)
+        self.drive.tank_drive(0, 0, scale)
+        time.sleep(3)
 
-def turntest(drive):
-    #turnrighttest and turnlefttest
-    for y in range(10):
-        scale = (y+1)/10
-        for x in range(1, 5):
-            turnrighttest(drive, x, scale)
-            turnlefttest(drive, x, scale)
+    def straighttestparams(self, duration=DURATION, scale=TEST_SCALE):
+        self.drive.tank_drive(0, 0, scale)
+        time.sleep(duration)
+        self.drive.tank_drive(1, 1, scale)
+        time.sleep(duration)
+        self.drive.tank_drive(0, 0, scale)
+        time.sleep(3)
 
-def straighttest(drive, duration, test_scale, turntime):
-    turntime = 2 #tested value for how long it takes to turn 180
-    #go straight and back test
-    for x in range(1,5):
-        straighttestparams(drive, duration, test_scale)
-        turnrighttest(drive, turntime, test_scale)
-        straighttestparams(drive, duration, test_scale)
-        turnrighttest(drive, turntime, test_scale)
+    def returntime(self):
+        return time.time()
 
-def straightspeed(drive, duration, test_scale, turntime):
-    #go straight and back test, straight speed
-    for x in range(10):
-        scale = (x+1)/10
-        straighttestparams(drive, duration, scale)
-        turnrighttest(drive, turntime, test_scale)
-        straighttestparams(drive, duration, scale)
-        turnrighttest(drive, turntime, test_scale)
+    def stop(self):
+        for i in range(8):
+            self.drive.tank_drive(0, 0, 0)
 
-if __name__ == '__main__':
-    main()
+    def turntest(self):
+        #turnrighttest and turnlefttest
+        for y in range(10):
+            scale = (y+1)/10
+            for x in range(1, 5):
+                self.turnrighttest(x, scale)
+                self.turnlefttest(x, scale)
+
+    def straighttest(self):
+        #turntime = 2 #tested value for how long it takes to turn 180
+        #go straight and back test
+        for x in range(1,5):
+            self.straighttestparams(DURATION, TEST_SCALE)
+            self.turnrighttest(TURNTIME, TEST_SCALE)
+            self.straighttestparams(DURATION, TEST_SCALE)
+            self.turnrighttest(TURNTIME, TEST_SCALE)
+
+    def straightspeed(self):
+        #go straight and back test, straight speed
+        for x in range(10):
+            scale = (x+1)/10
+            self.straighttestparams(DURATION, scale)
+            self.turnrighttest(TURNTIME, TEST_SCALE)
+            self.straighttestparams(DURATION, scale)
+            self.turnrighttest(TURNTIME, TEST_SCALE)
+
+#if __name__ == '__main__':
+#    main()
