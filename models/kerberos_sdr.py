1e803bc3790f32e6febaaacc127c81e18207d814
Samuel Stulman <samuel.stulman@berkeley.edu>
2021-04-24 14:17:23 -0700

    update doa method name

--- models/kerberos_sdr.py
+++ models/kerberos_sdr.py
@@ -5,6 +5,6 @@ class KerberosSDR:
         #want to write the value to desktop
         self.doa_fname = '/home/pi/Desktop/doa.txt'
 
-    def getDOA(self):
+    def get_DOA(self):
         doa_file = open(self.doa_fname, 'r')
         return doa_file.read()
