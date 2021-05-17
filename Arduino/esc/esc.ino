f9361de915a02d87bd97d9df22f4d85722fcea89
Leon Liu <leonliu22@berkeley.edu>
2021-05-16 20:13:01 -0700

    updated esc.ino

--- Arduino/esc/esc.ino
+++ Arduino/esc/esc.ino
@@ -4,8 +4,18 @@ byte servoPin1 = 5;
 Servo servo;
 Servo servo1;
 
+const int trigPin = 11;
+const int echoPin = 10;
+int iteration = 0;
+int sensor_counter = 0;
+// defines variables
+long duration;
+int distance;
+
 void setup() {
   // Initialize Serial communication to see printed values
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.begin(9600);
 
   servo.attach(servoPin);
@@ -19,6 +29,36 @@ void setup() {
 
 
 void loop() {
+  // put your main code here, to run repeatedly:
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance= duration*0.034/2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  if (distance < 100 && iteration > 100) {
+    if (sensor_counter > 10) {
+      while (true) {
+        servo.writeMicroseconds(1500);
+        servo1.writeMicroseconds(1500);
+      }
+    } else {
+      sensor_counter++;
+    }
+  } else {
+    sensor_counter = 0;
+  }
+  if (iteration < 101) {
+    iteration++;
+  }
   if (Serial.available() > 0) {
     String data = Serial.readStringUntil('\n');
     int servoVal = data[0] - 48;
